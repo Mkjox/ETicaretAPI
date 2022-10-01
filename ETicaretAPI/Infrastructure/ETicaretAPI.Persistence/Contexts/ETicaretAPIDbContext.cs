971a297db81d3e20c4cdbfa37e32171368043e57
Mert <zincir_890@hotmail.com>
2022-10-01 22:17:50 +0300

    Created Custom HttpClient Service

--- ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
@@ -28,7 +28,8 @@ namespace ETicaretAPI.Persistence.Contexts
                 _ = data.State switch
                 {
                     EntityState.Added => data.Entity.CreatedDate = DateTime.UtcNow,
-                    EntityState.Modified => data.Entity.UpdatedDate = DateTime.UtcNow
+                    EntityState.Modified => data.Entity.UpdatedDate = DateTime.UtcNow,
+                    _ => DateTime.UtcNow
                 };
             }
 
