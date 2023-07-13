e391d64548c5ef817bb68741c0cd252932780c5c
jox <zincir_890@hotmail.com>
2023-07-13 21:42:14 +0300

    Honestly, I have no idea what im doing rn

---
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Repositories/InvoiceFile/InvoiceFileReadRepository.cs
@@ -0,0 +1,17 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Repositories
+{
+    public class InvoiceFileReadRepository : ReadRepository<InvoiceFile>, IInvoiceFileReadRepository
+    {
+        public InvoiceFileReadRepository(ETicaretAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
