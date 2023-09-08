dd2e833e35d93fe3db752d48ae9c8ebbd0e8de41
jox <zincir_890@hotmail.com>
2023-09-08 04:03:05 +0300

    Changing the infrastructure for better management of file management

--- ETicaretAPI/Infrastructure/ETicaretAPI.Infrastructure/Services/FileService.cs
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Infrastructure/Services/FileService.cs
@@ -12,12 +12,7 @@ namespace ETicaretAPI.Infrastructure.Services
 {
     public class FileService : IFileService
     {
-        readonly IWebHostEnvironment _webHostEnvironment;
-
-        public FileService(IWebHostEnvironment webHostEnvironment)
-        {
-            _webHostEnvironment = webHostEnvironment;
-        }
+        
 
         public async Task<bool> CopyFileAsync(string path, IFormFile file)
         {
@@ -96,28 +91,7 @@ namespace ETicaretAPI.Infrastructure.Services
 
         public async Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection files)
         {
-            string uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, path);
-
-            if (!Directory.Exists(uploadPath))
-            {
-                Directory.CreateDirectory(uploadPath);
-            }
-
-            List<(string fileName, string path)> datas = new();
-            List<bool> results = new();
-            foreach (IFormFile file in files)
-            {
-                string fileNewName = await FileRenameAsync(uploadPath, file.FileName);
-
-                bool result = await CopyFileAsync($"{uploadPath}\\{fileNewName}", file);
-                datas.Add((fileNewName, $"{path}\\{fileNewName}"));
-                results.Add(result);
-            }
-
-            if (results.TrueForAll(r => r.Equals(true)))
-                return datas;
-
-            return null;
+            
 
             //todo Yukarıdaki if geçerli değilse dosyaların sunucuya yüklenmesinde bir hata oluştu şeklinde bir uyarıcı Exception eklenmesi gerekiyor
         }
---
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Infrastructure/Services/Storage/Local/LocalStorage.cs
@@ -0,0 +1,62 @@
+﻿using ETicaretAPI.Application.Abstraction.Storage.Local;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Infrastructure.Services.Storage.Local
+{
+    public class LocalStorage : ILocalStorage
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public LocalStorage(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public Task DeleteAsync(string path, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetFiles(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasFile(string path, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection files)
+        {
+            string uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, path);
+
+            if (!Directory.Exists(uploadPath))
+            {
+                Directory.CreateDirectory(uploadPath);
+            }
+
+            List<(string fileName, string path)> datas = new();
+            List<bool> results = new();
+            foreach (IFormFile file in files)
+            {
+
+                bool result = await CopyFileAsync($"{uploadPath}\\{fileNewName}", file);
+                datas.Add((fileNewName, $"{path}\\{fileNewName}"));
+                results.Add(result);
+            }
+
+            if (results.TrueForAll(r => r.Equals(true)))
+                return datas;
+
+            return null;
+        }
+    }
+}
